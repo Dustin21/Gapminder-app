41b09ff9ebd1c91e0dbedf1f5782c17ec3aca173
Dustin21 <dustin.bwj@gmail.com>
2014-11-17 10:53:30 -0800

    update

--- Gapminder-app/server.R
+++ Gapminder-app/server.R
@@ -5,9 +5,11 @@ gDat <- read.delim(file = gdURL)
 shinyServer(function(input, output){
 	
 	output$gapminder_table <- renderTable({
-		subset(gDat, gDat$country == input$select_country)
+		subset(gDat, country == input$select_country & 
+					 	year >= input$year_range[1] & year <= input$year_range[2])
 	})
 	output$output_country <- renderText({
+		cat(input$year_range)
 		paste("Country selected", input$select_country)
 	})
 })
--- Gapminder-app/ui.R
+++ Gapminder-app/ui.R
@@ -6,7 +6,14 @@ shinyUI(fluidPage (
 								 selectInput("select_country",
 								 						label = "Country",
 								 						choices = list("Chad", "Iraq", "Mali")
-								 						)
+								 						),
+								 sliderInput("year_range",
+								 						label = "Range of Years:",
+								 						
+								 						min = 1952,
+								 						max = 2007,
+								 						value = c(1955, 2005), format = "####",
+								 						step = 5)
 								 ),
 		mainPanel("My cool graphs will go here",
 							textOutput("output_country"),
