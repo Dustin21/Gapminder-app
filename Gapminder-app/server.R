97b8c5649c40fe395b0a99c5158e0b7a640a92ec
Dustin21 <dustin.bwj@gmail.com>
2014-11-27 16:08:38 -0800

    update

--- Gapminder-app/server.R
+++ Gapminder-app/server.R
@@ -1,4 +1,5 @@
 library(ggplot2);
+
 gdURL <- "http://tiny.cc/gapminder"
 gDat <- read.delim(file = gdURL)
 
@@ -15,9 +16,9 @@ shinyServer(function(input, output){
 		if(is.null(input$country_from_gapminder)) {
 			return(NULL)
 		}
-		
+	
 		subset(gDat, country == input$country_from_gapminder & 
-					 	year >= input$year_range[1] & year <= input$year_range[2] )
+					 	year >= input$year_range[1] & year <= input$year_range[2])
 	})
 	
 	output$gapminder_table <- renderTable({
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Gapminder-app
 =============
 
-Gapminder App for data selection
+This is a Shiny app that enables you to interactively select data pertaining to country and year, while analysing it through a plot.  
+
+Click [here](https://dustin21.shinyapps.io/Gapminder-app/) to begin using the app.
